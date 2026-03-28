0c4af3ea798f74afe24bd3322637ee1c616da599
Estelior <ll97@mail.ru>
2026-03-28 13:46:02 +0300

    Commit test and BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080"; //расширение браузера
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/tests/TextBoxTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest extends BaseTest {
+
+   @Test
+    void successFullFillFormTest() {
+       open("/text-box"); // открываем сайт
+       $("[id=userName]").setValue("Vera"); //ввести значение в поле
+       $("[id=userEmail]").setValue("fwwqg@mail.ru");
+       $("[id=currentAddress]").setValue("first address 1");
+       $("[id=permanentAddress]").setValue("second address 1"); //CTR+D - копирует строчки кода
+       $("[id=submit]").click(); //нажать кнопку
+
+       $("[id=output] [id=name]").shouldHave(text("Vera")); //должен быть текст
+       $("[id=output] [id=email]").shouldHave(text("fwwqg@mail.ru"));
+       $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
+       $("[id=output] [id=permanentAddress]").shouldHave(text("second address 1"));
+    }
+
+}
