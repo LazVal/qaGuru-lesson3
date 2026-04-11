2b461be8ee17ef0a7154318f81070d897222a124
Estelior <ll97@mail.ru>
2026-04-11 13:20:28 +0300

    Commit change name method

--- src/test/java/tests/homeWork3/BaseTest.java
+++ src/test/java/tests/homeWork3/BaseTest.java
@@ -1,27 +1,26 @@
 package tests.homeWork3;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationPage;
 import pages.RegistrationPageSecond;
-import pages.components.SubjectComponent;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
     RegistrationPage registrationPage = new RegistrationPage();
     RegistrationPageSecond registrationPageSecond = new RegistrationPageSecond();
 
     @BeforeAll
-    public static void beforeAll() {
+    public static void setupSelenideEnv() {
         Configuration.browserSize = "1920x1080"; //расширение браузера
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
     }
 
+
     @AfterEach
-    void afterEach() {
-        closeWebDriver();
+    void closeWebDriver() {
+        Selenide.closeWebDriver();
     }
 }
